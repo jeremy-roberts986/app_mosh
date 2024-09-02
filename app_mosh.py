3021e24fa7f851dec3678497f08b69941fa67f0c
jeremy-roberts986 <57444958+jeremy-roberts986@users.noreply.github.com>
2024-09-02 14:05:14 -0500

    Add files via upload

---
+++ app_mosh.py
@@ -0,0 +1,148 @@
+# price1 = 10
+# price = 20
+# rating = 4.9
+# name = 'Jeremy'
+# is_published = True
+# print(price)
+
+
+# name = 'John Smith'
+# age = 20
+# is_new_patient = True
+
+# name = input('What is your name? ')
+# color = input('What is your favorite color? ')
+# print(name + ' likes ' + color)
+
+# birth_year = input('Birth year: ')
+# print(type(birth_year))
+# age = 2019 - int(birth_year)
+# print(type(age))
+# print(age)
+
+# weight_pounds = input('What is your weight in pounds? ')
+# weight_kilos = float(weight_pounds) * 0.45
+# print(weight_kilos)
+
+# name = 'Jennifer'
+# print(name[1:-1])
+
+# first = 'John'
+# last = 'Smith'
+# # message = first + ' [' + last + '] is a coder'
+# msg = f"{first} {last} is a coder"
+# print(msg)
+
+# course = 'Python for beginners'
+# print(course.replace('beginners', 'Absolute Beginners'))
+# print('Python' in course)
+
+
+# x = (2 + 3) * 10 - 3
+# print(x)
+
+# import math
+#
+# print(math.floor(2.9))
+
+# hot_day = False
+# cold_day = False
+#
+# if hot_day:
+#     print("It's a hot day")
+#     print('Drink plenty of water')
+# elif cold_day:
+#     print("It's a cold day")
+#     print('Wear warm clothes')
+# else:
+#     print("It's a lovely day")
+#
+# print('Enjoy your day!')
+
+# price = 1000000
+# good_credit = True
+#
+# if good_credit:
+#    down_payment = 0.1 * price
+# else:
+#     down_payment= 0.2 * price
+# print(f"Down payment: ${down_payment}")
+
+# temperature = 35
+#
+# if temperature > 30:
+#     print("It's a hot day")
+# else:
+#     print("It's not a hot day")
+
+
+# name = 'John Smith'
+#
+# if len(name) < 3:
+#     print('Name must be at least 3 characters.')
+# elif len(name) > 50:
+#     print('Name must be a maximum of 50 characters.')
+# else:
+#     print('Name looks good!')
+
+# weight = int(input('Weight: '))
+# unit = input('(L)bs or (K)g: ')
+#
+# if unit.upper() == "L":
+#     converted = weight * 0.45
+#     print(f"You are {converted} kilos")
+# else:
+#     converted = weight / 0.45
+#     print(f"You are {converted} pounds")
+
+# guess_count = 1
+
+# while i <= 5:
+#     print('*' * i)
+#     i = i +1
+# print('Done')
+
+#
+# secret_number = 9
+# guess_count = 0
+# guess_limit = 3
+#
+# while guess_count < guess_limit:
+#     guess = int(input('Guess: '))
+#     guess_count += 1
+#     if guess == secret_number:
+#         print('You won!')
+#         break
+# else:
+#     print('You failed.')
+
+# command = ""
+# started = False
+# while True:
+#     command = input("> ").lower()
+#     if command == "start":
+#         if started:
+#             print("Car is already started.")
+#         else:
+#             started = True
+#             print("Car started.")
+#     elif command == "stop":
+#         if not started:
+#             print('Car is already stopped.')
+#         else:
+#             started = False
+#             print('Car stopped.')
+#     elif command == 'help':
+#         print("""
+# Start - start the car
+# Stop - stop the car
+# Quit - to quit""")
+#     elif command == "quit":
+#         break
+#     else:
+#         print("Sorry, I don't understand.")
+
+
+
+
+
